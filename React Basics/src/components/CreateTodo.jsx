d5853ee97f1a02db7eec2dcc7edce9ecccec7f66
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-01 06:14:15 +0530

    setting element attributes

--- React Basics/src/components/CreateTodo.jsx
+++ React Basics/src/components/CreateTodo.jsx
@@ -1,12 +1,15 @@
 const CreateTodo = () => {
   const tasks = 5;
+  const hideButton = false;
   const countTasks = () => {
     return tasks === 0 ? "No task available" : `Tasks: ${tasks}`;
   };
   return (
     <>
-      <h2>{countTasks()}</h2>
-      <button>Add Task</button>
+      <h2 style={{ backgroundColor: "lightblue" }}>{countTasks()}</h2>
+      <button className="btn" disabled={hideButton}>
+        Add Task
+      </button>
     </>
   );
 };
