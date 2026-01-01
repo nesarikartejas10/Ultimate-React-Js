ca716f18d8201862162a6b6c2397538262af8806
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-01 07:16:20 +0530

    handling user input

--- React Basics/src/components/CreateTodo.jsx
+++ React Basics/src/components/CreateTodo.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const CreateTodo = () => {
   const [tasks, setTasks] = useState(0);
+  const [input, setInput] = useState("");
 
   const hideButton = false;
   const countTasks = () => {
@@ -11,12 +12,18 @@ const CreateTodo = () => {
   const handleClick = () => {
     setTasks(tasks + 1);
   };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
   return (
     <>
       <h2 style={{ backgroundColor: "lightblue" }}>{countTasks()}</h2>
+      <input type="text" onChange={handleChange} />
       <button className="btn" disabled={hideButton} onClick={handleClick}>
         Add Task
       </button>
+      <h2>{input}</h2>
     </>
   );
 };
