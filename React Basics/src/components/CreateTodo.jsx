1195150d0fb7f334d3a499b80b0de9131187fb95
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-01 06:30:19 +0530

    add events

--- React Basics/src/components/CreateTodo.jsx
+++ React Basics/src/components/CreateTodo.jsx
@@ -1,13 +1,18 @@
 const CreateTodo = () => {
-  const tasks = 5;
+  let tasks = 0;
   const hideButton = false;
   const countTasks = () => {
     return tasks === 0 ? "No task available" : `Tasks: ${tasks}`;
   };
+
+  const handleClick = () => {
+    tasks++;
+    console.log("Add task:", tasks);
+  };
   return (
     <>
       <h2 style={{ backgroundColor: "lightblue" }}>{countTasks()}</h2>
-      <button className="btn" disabled={hideButton}>
+      <button className="btn" disabled={hideButton} onClick={handleClick}>
         Add Task
       </button>
     </>
