7e3ba7deb574bbde3340e1051b084cd72f95746b
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-02 06:46:12 +0530

    added style for form component

--- TaskTrek Project/src/App.jsx
+++ TaskTrek Project/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Taskform from "./components/TaskForm";
 
 export default function App() {
   return (
     <div className="app">
+      <Taskform />
       <main className="app_main">
         <section className="task_column">section 1</section>
         <section className="task_column">section 2</section>
--- TaskTrek Project/src/components/TaskForm.jsx
+++ TaskTrek Project/src/components/TaskForm.jsx
@@ -1,3 +1,5 @@
+import "../styles/TaskForm.css";
+
 export default function Taskform() {
   return (
     <header className="app_header">
@@ -5,25 +7,28 @@ export default function Taskform() {
         <input
           type="text"
           className="task_input"
-          placeholder="enter your task"
+          placeholder="Enter your task"
         />
 
         <div className="task_form_bottom_line">
-          <button className="tag">HTML</button>
-          <button className="tag">CSS</button>
-          <button className="tag">JavaScript</button>
-          <button className="tag">React</button>
+          <div>
+            <button className="tag">HTML</button>
+            <button className="tag">CSS</button>
+            <button className="tag">JavaScript</button>
+            <button className="tag">React</button>
+          </div>
+
+          <div>
+            <select className="task_status">
+              <option value="todo">To Do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+            <button className="task_submit" type="submit">
+              + Add Task
+            </button>
+          </div>
         </div>
-
-        <select className="task_status">
-          <option value="todo">To Do</option>
-          <option value="doing">Doing</option>
-          <option value="done">Done</option>
-        </select>
-
-        <button className="task_submit" type="submit">
-          + Add Task
-        </button>
       </form>
     </header>
   );
