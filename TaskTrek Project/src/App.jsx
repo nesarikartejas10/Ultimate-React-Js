371aea278e37d38f4fc37e9f63c79bdc06b2a09a
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-02 06:06:19 +0530

    creating website layout

--- TaskTrek Project/src/App.jsx
+++ TaskTrek Project/src/App.jsx
@@ -1,3 +1,14 @@
+import "./App.css";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <header className="app_header">Header Section</header>
+      <main className="app_main">
+        <section className="task_column">section 1</section>
+        <section className="task_column">section 2</section>
+        <section className="task_column">section 3</section>
+      </main>
+    </div>
+  );
 }
