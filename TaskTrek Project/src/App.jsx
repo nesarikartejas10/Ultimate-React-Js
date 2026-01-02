3225ba9d9569be05ac310622a9f4b17ac7103785
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-02 06:17:39 +0530

    task form component created

--- TaskTrek Project/src/App.jsx
+++ TaskTrek Project/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 export default function App() {
   return (
     <div className="app">
-      <header className="app_header">Header Section</header>
       <main className="app_main">
         <section className="task_column">section 1</section>
         <section className="task_column">section 2</section>
---
+++ TaskTrek Project/src/components/TaskForm.jsx
@@ -0,0 +1,30 @@
+export default function Taskform() {
+  return (
+    <header className="app_header">
+      <form>
+        <input
+          type="text"
+          className="task_input"
+          placeholder="enter your task"
+        />
+
+        <div className="task_form_bottom_line">
+          <button className="tag">HTML</button>
+          <button className="tag">CSS</button>
+          <button className="tag">JavaScript</button>
+          <button className="tag">React</button>
+        </div>
+
+        <select className="task_status">
+          <option value="todo">To Do</option>
+          <option value="doing">Doing</option>
+          <option value="done">Done</option>
+        </select>
+
+        <button className="task_submit" type="submit">
+          + Add Task
+        </button>
+      </form>
+    </header>
+  );
+}
