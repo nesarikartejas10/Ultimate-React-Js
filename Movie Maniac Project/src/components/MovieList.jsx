d3a29a75d3da9f5d7e593c85694a40575f13abc4
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-04 20:27:50 +0530

    implement sort feature

--- Movie Maniac Project/src/components/MovieList.jsx
+++ Movie Maniac Project/src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import "../styles/MovieList.css";
+import _ from "lodash";
 import Fire from "../assets/fire.png";
 import MovieCard from "./MovieCard";
 import { useEffect, useState } from "react";
@@ -8,6 +9,11 @@ export default function MovieList() {
   const [movies, setMovies] = useState([]);
   const [filterMovies, setFilterMovies] = useState([]);
   const [rating, setRating] = useState(0);
+  const [sort, setSort] = useState({
+    by: "default",
+    order: "asc",
+  });
+
   useEffect(() => {
     try {
       fetchMovies();
@@ -15,6 +21,13 @@ export default function MovieList() {
       console.log("Error while fetching movies", error);
     }
   }, []);
+
+  useEffect(() => {
+    if (sort.by !== "default") {
+      const sortedMovies = _.orderBy(filterMovies, [sort.by], [sort.order]);
+      setFilterMovies(sortedMovies);
+    }
+  }, [sort]);
 
   const fetchMovies = async () => {
     const response = await fetch(
@@ -39,6 +52,13 @@ export default function MovieList() {
     }
   };
 
+  const handleSort = (e) => {
+    const { name, value } = e.target;
+    setSort((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
   return (
     <section className="movie_list">
       <header className="movie_list_header">
@@ -53,15 +73,25 @@ export default function MovieList() {
             ratings={[8, 7, 6]}
           />
 
-          <select name="" className="movie_sort">
-            <option value="">Sort By</option>
-            <option value="">Date</option>
-            <option value="">Rating</option>
+          <select
+            name="by"
+            className="movie_sort"
+            onChange={handleSort}
+            value={sort.by}
+          >
+            <option value="default">Sort By</option>
+            <option value="release_date">Date</option>
+            <option value="vote_average">Rating</option>
           </select>
 
-          <select name="" className="movie_sort">
-            <option value="">a-z</option>
-            <option value="">z-a</option>
+          <select
+            name="order"
+            className="movie_sort"
+            onChange={handleSort}
+            value={sort.order}
+          >
+            <option value="asc">a-z</option>
+            <option value="desc">z-a</option>
           </select>
         </div>
       </header>
