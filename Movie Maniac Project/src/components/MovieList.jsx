ce7caf9f61bd2cda5ec862f11e21d5923cd37bfd
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-04 19:41:44 +0530

    implement filter feature

--- Movie Maniac Project/src/components/MovieList.jsx
+++ Movie Maniac Project/src/components/MovieList.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function MovieList() {
   const [movies, setMovies] = useState([]);
+  const [filterMovies, setFilterMovies] = useState([]);
+  const [rating, setRating] = useState(0);
   useEffect(() => {
     try {
       fetchMovies();
@@ -20,6 +22,20 @@ export default function MovieList() {
 
     const data = await response.json();
     setMovies(data.results);
+    setFilterMovies(data.results);
+  };
+
+  const handleFilter = (rate) => {
+    if (rate === rating) {
+      setRating(0);
+      setFilterMovies(movies);
+    } else {
+      setRating(rate);
+      const filteredMovies = movies.filter(
+        (movie) => movie.vote_average >= rate
+      );
+      setFilterMovies(filteredMovies);
+    }
   };
 
   return (
@@ -31,9 +47,30 @@ export default function MovieList() {
 
         <div className="movie_list_fs">
           <ul className="movie_filter">
-            <li className="movie_filter_item active">8+ Star</li>
-            <li className="movie_filter_item">7+ Star</li>
-            <li className="movie_filter_item">6+ Star</li>
+            <li
+              className={
+                rating === 8 ? "movie_filter_item active" : "movie_filter_item"
+              }
+              onClick={() => handleFilter(8)}
+            >
+              8+ Star
+            </li>
+            <li
+              className={
+                rating === 7 ? "movie_filter_item active" : "movie_filter_item"
+              }
+              onClick={() => handleFilter(7)}
+            >
+              7+ Star
+            </li>
+            <li
+              className={
+                rating === 6 ? "movie_filter_item active" : "movie_filter_item"
+              }
+              onClick={() => handleFilter(6)}
+            >
+              6+ Star
+            </li>
           </ul>
 
           <select name="" className="movie_sort">
@@ -50,9 +87,13 @@ export default function MovieList() {
       </header>
 
       <div className="movie_cards">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {filterMovies.length === 0 ? (
+          <h3 className="not_found_msg">Oops! No movies found...</h3>
+        ) : (
+          filterMovies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))
+        )}
       </div>
     </section>
   );
