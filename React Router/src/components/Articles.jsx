9f4fa28c6570f3d7c96c20ea6e4239358834b16b
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-08 22:35:01 +0530

    get query string with usrlSearchParams hook

--- React Router/src/components/Articles.jsx
+++ React Router/src/components/Articles.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Articles() {
-  return <div>Articles</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy");
+  const category = searchParams.get("category");
+
+  const handleSort = () => {
+    setSearchParams({
+      sortBy: "views",
+      category,
+    });
+  };
+
+  return (
+    <div>
+      <h2>
+        Articles
+        <p>
+          SortBy:{sortBy} category:{category}
+        </p>
+      </h2>
+
+      <button onClick={handleSort}>Sort by views</button>
+    </div>
+  );
 }
 
 export default Articles;
