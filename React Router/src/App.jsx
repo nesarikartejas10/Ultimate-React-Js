340addec7a0ec222bb177d99070381208b49813a
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-08 19:56:14 +0530

    setting up prohect fot react router

---
+++ React Router/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="app_main"></main>
+    </div>
+  );
+}
+
+export default App;
---
+++ React Router/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/products">Products</a>
+        </li>
+        <li>
+          <a href="/articles">Articles</a>
+        </li>
+        <li>
+          <a href="/admin">Admin</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
