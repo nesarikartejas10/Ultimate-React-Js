720cb1345c00c37e4a5814721f602117e8866036
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-05 21:47:34 +0530

    add not found page

--- React_Query/src/App.jsx
+++ React_Query/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import Articles from "./pages/articles/Articles";
 import Admin from "./pages/admin/Admin";
+import NotFound from "./pages/not-found/NotFound";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/products" element={<Products />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </div>
---
+++ React_Query/src/pages/not-found/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <h2 style={{ color: "lightcoral" }}>OOPs | 404 page not found!!</h2>;
+};
+
+export default NotFound;
