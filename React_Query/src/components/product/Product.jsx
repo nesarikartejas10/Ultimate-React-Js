f57dce0fb0d49e0085b1ca0acb6af991592e9905
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-06 09:45:46 +0530

    add query string

--- React_Query/src/components/product/Product.jsx
+++ React_Query/src/components/product/Product.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Product = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   return (
     <div>
       <h2>Product: {id}</h2>
+      <button onClick={() => navigate("/products")}>Go Back to products</button>
     </div>
   );
 };
--- React_Query/src/pages/articles/Articles.jsx
+++ React_Query/src/pages/articles/Articles.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Articles = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy");
+  const category = searchParams.get("category");
+
+  function handleSortBy() {
+    setSearchParams({ sortBy: "views", category: category });
+  }
   return (
     <div>
       <h2>Articles</h2>
+      <p>Sort By: {sortBy}</p>
+      <p>Categoty: {category}</p>
+      <button onClick={handleSortBy}>Sort by views</button>
     </div>
   );
 };
