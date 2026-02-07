8b873e493907fed1a21b9151bc689ff7f4e4d65d
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-07 11:44:50 +0530

    add new seller data

--- React_Query/src/pages/admin/Sellers.jsx
+++ React_Query/src/pages/admin/Sellers.jsx
@@ -6,6 +6,7 @@ const Sellers = () => {
   const [sellers, setSellers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [sellerInput, setSellerInput] = useState("");
 
   useEffect(() => {
     setLoading(true);
@@ -20,13 +21,39 @@ const Sellers = () => {
         setError(error.message);
       });
   }, []);
+
+  const addSeller = () => {
+    const newSeller = {
+      name: sellerInput,
+      id: sellers.length + 1,
+    };
+
+    setSellers([newSeller, ...sellers]);
+    setSellerInput("");
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newSeller)
+      .then((res) => setSellers([res.data, ...sellers]))
+      .catch((error) => {
+        setError(error.message);
+        setSellers(sellers);
+      });
+  };
+
   return (
     <>
       <h3>Admin Sellers Page</h3>
+      <div>
+        <input
+          type="text"
+          placeholder="seller name"
+          value={sellerInput}
+          onChange={(e) => setSellerInput(e.target.value)}
+        />
+        <button onClick={addSeller}>Add Seller</button>
+      </div>
       {loading && <Loader />}
-      {error && (
-        <em style={{ color: "coral" }}>{error}!!Please kindly check url!!</em>
-      )}
+      {error && <em style={{ color: "coral" }}>{error}!!</em>}
       {sellers.map((seller) => {
         return <p key={seller.id}>{seller.name}</p>;
       })}
