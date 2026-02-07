a8af185513e65ae2a5a066c3d47827054f4b1f9e
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-07 10:31:31 +0530

    handle error state

--- React_Query/src/pages/admin/Sellers.jsx
+++ React_Query/src/pages/admin/Sellers.jsx
@@ -5,18 +5,28 @@ import Loader from "../../components/loader/Loader";
 const Sellers = () => {
   const [sellers, setSellers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setLoading(true);
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      setSellers(res.data);
-      setLoading(false);
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setSellers(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error.message);
+      });
   }, []);
   return (
     <>
       <h3>Admin Sellers Page</h3>
       {loading && <Loader />}
+      {error && (
+        <em style={{ color: "coral" }}>{error}!!Please kindly check url!!</em>
+      )}
       {sellers.map((seller) => {
         return <p key={seller.id}>{seller.name}</p>;
       })}
