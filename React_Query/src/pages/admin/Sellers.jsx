b1ee46311db69a68c0f49361319761521a5a2999
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-07 10:21:15 +0530

    add loading indicator

--- React_Query/src/pages/admin/Sellers.jsx
+++ React_Query/src/pages/admin/Sellers.jsx
@@ -1,17 +1,22 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loader from "../../components/loader/Loader";
 
 const Sellers = () => {
   const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setSellers(res.data));
+    setLoading(true);
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setSellers(res.data);
+      setLoading(false);
+    });
   }, []);
   return (
     <>
       <h3>Admin Sellers Page</h3>
+      {loading && <Loader />}
       {sellers.map((seller) => {
         return <p key={seller.id}>{seller.name}</p>;
       })}
